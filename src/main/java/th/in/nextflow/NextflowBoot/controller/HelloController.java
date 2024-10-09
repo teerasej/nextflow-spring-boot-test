aa253f951c94a5c9a8a450e18fb98365382a9cbc
Teerasej <teerasej@gmail.com>
2024-10-09 20:46:34 +0700

    create controller and provide a test method

---
+++ src/main/java/th/in/nextflow/NextflowBoot/controller/HelloController.java
@@ -0,0 +1,28 @@
+package th.in.nextflow.NextflowBoot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Message sayHello() {
+        return new Message("Hello, World!");
+    }
+
+    public static class Message {
+        private String message;
+
+        public Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
